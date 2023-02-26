76a586fc8f9a3e4cc1056d5c81890aab3d59bd23
FairestW <92035064+FairestW@users.noreply.github.com>
2023-02-26 19:40:49 +0000

    Add codacy badge

--- cash.cpp
+++ cash.cpp
@@ -40,4 +40,4 @@ int main(void)
         }
     }
     int sum = pennies + nickels + dimes + quarters;
-    //std::cout << pennies << ""\n" << nickels
+    //std::cout << pennies << ""\n" << nickels 
