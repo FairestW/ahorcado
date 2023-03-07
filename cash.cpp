ba06e8010290318392860ff2a1cb58df4c1e7985
FairestW <92035064+FairestW@users.noreply.github.com>
2023-03-07 19:15:06 +0000

    another cleanup

--- cash.cpp
+++ cash.cpp
@@ -40,4 +40,4 @@ int main(void)
         }
     }
     int sum = pennies + nickels + dimes + quarters;
-    //std::cout << pennies << ""\n" << nickels << "\n" << dimes << "\n" 
+    //std::cout << pennies << ""\n" << nickels << "\n" << dimes << "\n" <<
