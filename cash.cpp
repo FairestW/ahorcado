ca0eef4adf98c33700f0f257750d6f174fec1e49
FairestW <92035064+FairestW@users.noreply.github.com>
2023-03-09 08:17:02 +0000

    AI-3.1.2 <cszdz@DESKTOP-CVI6GSM Update KeyPromoterXStatistic.xml

--- cash.cpp
+++ cash.cpp
@@ -40,4 +40,4 @@ int main(void)
         }
     }
     int sum = pennies + nickels + dimes + quarters;
-    //std::cout << pennies << ""\n" << nickels << "\n" << dimes << "\n" << quar
+    //std::cout << pennies << ""\n" << nickels << "\n" << dimes << "\n" << quarter
