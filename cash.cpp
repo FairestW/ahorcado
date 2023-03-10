89e59529abe72bca4e2c30a0b06ef2b931bfce7c
FairestW <92035064+FairestW@users.noreply.github.com>
2023-03-10 11:49:09 +0000

    fixed types

--- cash.cpp
+++ cash.cpp
@@ -40,4 +40,4 @@ int main(void)
         }
     }
     int sum = pennies + nickels + dimes + quarters;
-    //std::cout << pennies << ""\n" << nickels << "\n" << dimes << "\n" << quarter
+    //std::cout << pennies << ""\n" << nickels << "\n" << dimes << "\n" << quarters
