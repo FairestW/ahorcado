309517f5151a96287ec40bf00a02b0ada64a42d6
FairestW <92035064+FairestW@users.noreply.github.com>
2023-03-25 18:57:20 +0000

    Added copyright notice.

--- cash.cpp
+++ cash.cpp
@@ -41,4 +41,4 @@ int main(void)
     }
     int sum = pennies + nickels + dimes + quarters;
     //std::cout << pennies << ""\n" << nickels << "\n" << dimes << "\n" << quarters << "\n";
-    //prints the sum of all the c
+    //prints the sum of all the co
